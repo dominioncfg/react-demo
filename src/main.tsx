df73e3c6b426cc292bf231b9264408101cd5437a
Jose Carlos <josecdom94@gmail.com>
2025-10-18 16:11:59 +0200

    Sonner

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './App.css'
-import { MyRoutedApp } from './03.reactRouter/MyRouterApp'
+import { MySonnerApp } from './04.sonner/MySonnerApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-        <MyRoutedApp/>
+    <MySonnerApp />
   </StrictMode>,
 )
